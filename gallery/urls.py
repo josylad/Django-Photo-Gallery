d47b1f8a96563f1a53419ede997c533adaf40137
Joseph Adediji <josylado@gmail.com>
2019-10-06 14:51:28 +0300

    Add URL regex for image page

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
     url(r'^search/', views.search_images, name='search_results'),
+    url(r'^image/(\d+)', views.get_image, name='image_results'),
     ]
