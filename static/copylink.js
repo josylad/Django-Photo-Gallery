4290383764066160b3365c75cba173bef2ff945a
Joseph Adediji <josylado@gmail.com>
2019-10-06 15:00:56 +0300

    edit JS file and add gallery modal popup script

--- static/copylink.js
+++ static/copylink.js
@@ -7,3 +7,15 @@ $(function() {
     });
    });
 
+
+   // init Masonry
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });
