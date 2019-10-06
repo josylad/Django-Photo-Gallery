2d1af22434d35d1af95ac821050d8dc90909df73
Joseph Adediji <josylado@gmail.com>
2019-10-06 14:49:45 +0300

    Add Image test case

--- gallery/tests.py
+++ gallery/tests.py
@@ -41,3 +41,22 @@ class LocationTestCase(TestCase):
         self.assertTrue(len(all_locations) > 0)
         
         
+class ImageTestCase(TestCase):
+    
+    def setUp(self):
+        # Creating a new location and saving it
+        self.new_location = Location(location = 'Lagos')
+        self.new_location.save()
+        
+        self.category = Category(category = 'Animals')
+        self.category.save()
+        
+        # Creating a new Image and saving it
+        self.image= Image(image_name = 'Beagle', description ='Lovely dog', image_file ='images/beagle.jpg', location = self.new_location, category = self.category)
+        self.image.save_image()
+
+
+    def test_save_images(self):
+        self.image.save_image()
+        all_images = Image.objects.all()
+        self.assertTrue(len(all_images) > 0)
