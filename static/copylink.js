af4d4ea4d9f723269d8086b942a7d9a940f5e510
Joseph Adediji <josylado@gmail.com>
2019-10-06 15:00:32 +0300

    edit JS file and add link copy function

---
+++ static/copylink.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('input').click(function() {
+    $(this).focus();
+    $(this).select();
+    document.execCommand('copy');
+    $(this).after("Copied to clipboard");
+    });
+   });
+
