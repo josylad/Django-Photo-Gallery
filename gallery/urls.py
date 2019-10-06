965ea9b43d98ca34288297d6867c1634d1e88a29
Joseph Adediji <josylado@gmail.com>
2019-10-06 14:51:46 +0300

    Add URL regex for location  page

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name= 'index'),
     url(r'^search/', views.search_images, name='search_results'),
     url(r'^image/(\d+)', views.get_image, name='image_results'),
-    ]
+    url(r'^location/(?P<location>\w{0,50})/', views.location, name='location_results'),
+]
