14d2f876235db4a368b7b35a728b6a0d9f43ed99
Joseph Adediji <josylado@gmail.com>
2019-10-06 14:50:51 +0300

    Add URL regex for home page

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,10 +6,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
-    url(r'^search/', views.search_images, name='search_results'),
-    url(r'^image/(\d+)', views.get_image, name='image_results'),
-    url(r'^location/(?P<location>\w{0,50})/', views.location, name='location_results'),
-]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    ]
